bfc1b67c1d9a49db6c5185a2d41aa2b0a30742c8
Will <wdputnam42@gmail.com>
2024-04-10 02:17:06 -0400

    completed fetch calls for weather data. todo: find what data from the results that I would like the user to see on the page.

--- assets/script.js
+++ assets/script.js
@@ -1,22 +1,34 @@
 console.log("ITS GON RAIN!")
 
-var cityNameInput = document.querySelector('#input-box')
-var searchBtn = document.getElementById('search-btn')
+var cityNameInput = document.querySelector('#input-box');
+var searchBtn = document.getElementById('search-btn');
 
 
 function getLongLat() {
 
+    
+
     var getCity = cityNameInput.value
     var url = "http://api.openweathermap.org/geo/1.0/direct?q=" + getCity + "&limit=1&appid=8ae331534e5d504cddf6268ac4ef3357" 
-
     fetch(url)
     .then(function (response) {
         return response.json();
     })
     .then(function (data) {
         console.log(data);
+        const lonData = data[0].lon;
+        const latData = data[0].lat;
+
+        return fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${latData}&lon=${lonData}&appid=8ae331534e5d504cddf6268ac4ef3357`)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+        })
     })
-    return;
+    
+
 }
 
 
